b34184ea0cc6ef479a9915aa66262cc77823c5cd
Phuoc Nguyen <vodanh.2901@gmail.com>
2023-02-15 19:03:11 +0700

    fix tap

--- lib/dino_game.dart
+++ lib/dino_game.dart
@@ -1,63 +1,100 @@
 import 'dart:ui';
 
 import 'package:flame/effects.dart';
+import 'package:flame/events.dart';
 import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
 import 'package:reno_bar/bar_center.dart';
 import 'package:reno_bar/dino_player.dart';
 import 'package:reno_bar/dino_world.dart';
 
-class DinoGame extends FlameGame with HasTappableComponents {
-  final DinoPlayer _dinoPlayer = DinoPlayer();
-  final DinoWorld _dinoWorld = DinoWorld();
-   final BarCenter _barCenter = BarCenter();
-   final BarLeft _barLeft = BarLeft();
-   final BarRight _barRight = BarRight();
+class DinoGame extends FlameGame with HasTappables {
+  final DinoPlayer dinoPlayer = DinoPlayer();
+  final DinoWorld dinoWorld = DinoWorld();
+   final BarCenter barCenter = BarCenter();
+   final BarLeft barLeft = BarLeft();
+   final BarRight barRight = BarRight();
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    await add(_dinoWorld);
-    await add(_dinoPlayer);
+    await add(dinoWorld);
+    await add(dinoPlayer);
     // await add(_barCenter);
-    await add(_barLeft);
-    await add(_barRight);
-    _dinoPlayer.position =
-        Vector2(_dinoWorld.size.x / 2.5, _dinoWorld.size.y / 1.3);
+    await add(barLeft);
+    await add(barRight);
+    dinoPlayer.position =
+        Vector2(dinoWorld.size.x / 2.5, dinoWorld.size.y / 1.3);
     camera.followComponent(
-      _dinoPlayer,
-      worldBounds: Rect.fromLTRB(0, 0, _dinoWorld.size.x, _dinoWorld.size.y),
+      dinoPlayer,
+      worldBounds: Rect.fromLTRB(0, 0, dinoWorld.size.x, dinoWorld.size.y),
     );
   }
 
+  void test(){
+    print('test');
+  }
 
   @override
-  void onTapDown(TapDownEvent event) {
-    super.onTapDown(event);
-    RemoveEffect effect = RemoveEffect(delay: 0);
+  void onTapUp(int pointerId, TapUpInfo info) {
+    super.onTapUp(pointerId, info);
+    if (info.handled) {
+      print('up');
+      if(children.contains(barCenter)){
+        barCenter.add(RemoveEffect(delay: 0));
+      }
 
-    if(!children.contains(_barCenter)){
-      add(_barCenter);
+      if(!(children.contains(barLeft) || children.contains(barRight))){
+        add(barLeft);
+        add(barRight);
+      }
     }
+  }
 
-    if((children.contains(_barLeft) || children.contains(_barRight))){
-      _barLeft.add(RemoveEffect(delay: 0));
-      _barRight.add(RemoveEffect(delay: 0));
+  @override
+  void onTapDown(int pointerId, TapDownInfo info) {
+    super.onTapDown(pointerId, info);
+    if (info.handled) {
+      print('down');
+      RemoveEffect effect = RemoveEffect(delay: 0);
+      if(!children.contains(barCenter)){
+        add(barCenter);
+      }
+      if((children.contains(barLeft) || children.contains(barRight))){
+        barLeft.add(RemoveEffect(delay: 0));
+        barRight.add(RemoveEffect(delay: 0));
+      }
+
     }
 
   }
 
-  @override
-  void onTapUp(TapUpEvent event) {
-    super.onTapUp(event);
 
-    if(children.contains(_barCenter)){
-      _barCenter.add(RemoveEffect(delay: 0));
-    }
-
-    if(!(children.contains(_barLeft) || children.contains(_barRight))){
-      add(_barLeft);
-      add(_barRight);
-    }
-  }
+  // @override
+  // void onTapDown(TapDownEvent event) {
+  //   super.onTapDown(event);
+  //   RemoveEffect effect = RemoveEffect(delay: 0);
+  //   if(!children.contains(_barCenter)){
+  //     add(_barCenter);
+  //   }
+  //   if((children.contains(_barLeft) || children.contains(_barRight))){
+  //     barLeft.add(RemoveEffect(delay: 0));
+  //     barRight.add(RemoveEffect(delay: 0));
+  //   }
+  //
+  // }
+  //
+  // @override
+  // void onTapUp(TapUpEvent event) {
+  //   super.onTapUp(event);
+  //
+  //   if(children.contains(_barCenter)){
+  //     barCenter.add(RemoveEffect(delay: 0));
+  //   }
+  //
+  //   if(!(children.contains(_barLeft) || children.contains(_barRight))){
+  //     add(_barLeft);
+  //     add(_barRight);
+  //   }
+  // }
 
 }
--- lib/dino_player.dart
+++ lib/dino_player.dart
@@ -1,29 +1,44 @@
 
 
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flame/events.dart';
 import 'package:flame/experimental.dart';
+import 'package:reno_bar/bar_center.dart';
 
-class DinoPlayer extends SpriteComponent with HasGameRef, TapCallbacks {
+import 'dino_game.dart';
+
+class DinoPlayer extends SpriteComponent with HasGameRef<DinoGame>, Tappable {
 
   DinoPlayer() : super(size: Vector2.all(100));
-
 
  @override
   Future<void> onLoad() async {
    super.onLoad();
-   sprite = await gameRef.loadSprite('Saw.png');
-   position = gameRef.size /3 ;
+   sprite = await gameRef.loadSprite('Barrel (1).png');
+   // position = gameRef.size /3 ;
+   // position = Vector2(0, 0);
  }
 
- // @override
- // void onTapUp(TapUpEvent event){
- //  print('tap up');
- //  print(event);
- // }
- // @override
- // void onTapDown(TapDownEvent event){
- //  print('tap down');
- // }
+ @override
+ bool onTapDown(TapDownInfo info) {
+    // TODO: implement onTapDown
+    // print('dino');
+    // print(info.eventPosition.viewport);
+    // print(info.eventPosition.game);
+    // print(info.eventPosition.global);
+    // print(info.eventPosition.widget);
+    // print(info.handled);
+   info.handled = true;
+    return true;
+  }
+
+  @override
+  bool onTapUp(TapUpInfo info) {
+    info.handled = true;
+    return true;
+  }
+
 
  // @override
  // void onLongTapDown(TapDownEvent event){
